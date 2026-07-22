3ad0b5ed107cf0d32795acfb56e17c4a5ca11483
Teddy-Martins <tmartins2@ivytech.edu>
2026-07-22 14:52:05 -0400

    Fundamental concepts of Python - IDLE

---
+++ My first Python program.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print("I just wrote my first Python Program")
+print("This is so much fun")
+
+#This is setting all the variables before I run the program
+c = "cats"
+d = "dogs."
+s = "It is raining " + c + " and " + d
+print(s)
