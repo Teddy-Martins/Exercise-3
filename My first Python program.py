1b48516a810ef6e036d3034cc6707c4f2d8d899b
Teddy-Martins <tmartins2@ivytech.edu>
2026-07-22 14:53:25 -0400

    Edited comments

--- My first Python program.py
+++ My first Python program.py
@@ -2,7 +2,7 @@ print("Hello World")
 print("I just wrote my first Python Program")
 print("This is so much fun")
 
-#This is setting all the variables before I run the program
+#This is setting all the variables before running the program
 c = "cats"
 d = "dogs."
 s = "It is raining " + c + " and " + d
